b4afb2c6edc63334b82d3a02423d2909068a33b8
Marko Tintor <marko@tintor.net>
2020-07-04 17:08:11 -0700

    core: add [array_deque] (works for non-trivial types)

--- core/array_deque.h
+++ core/array_deque.h
@@ -1,85 +1,266 @@
 #pragma once
-#include <type_traits>
-#include <vector>
+#include <cstdlib>
+#include <new>
 
+#include "core/bits_util.h"
 #include "core/numeric.h"
 
-template<typename T, class Enable = void>
-class array_deque; // undefined if T is not trivial type!
-
-template <typename T>
-class array_deque<T, std::enable_if_t<std::is_trivial_v<T>>> {
+template<typename T>
+class raw_array {
    public:
-    array_deque() {}
-    array_deque(uint capacity) : m_data(capacity) {}
+    raw_array() : _data(nullptr) {}
 
-    array_deque(const array_deque& o) {
-        m_data.resize(o.capacity());
-        m_start = 0;
-        m_size = o.size();
-        for (uint i = 0; i < m_size; i++) {
-            operator[](i) = o[i];
-        }
+    raw_array(size_t size, bool zero_init = false) {
+        _data = reinterpret_cast<T*>(zero_init ? calloc(sizeof(T), size) : malloc(sizeof(T) * size));
+        if (_data == nullptr) throw std::bad_alloc();
     }
 
-    auto operator=(const array_deque& o) {
-        if (this == &o) {
-            return *this;
-        }
-        if (o.size() > capacity()) {
-            m_data.resize(round_up_power2(o.size()));
-        }
-        m_start = 0;
-        m_size = o.size();
-        for (uint i = 0; i < m_size; i++) {
-            operator[](i) = o[i];
+    raw_array(const raw_array&) = delete;
+
+    raw_array(raw_array&& e) {
+        _data = e._data;
+        e._data = nullptr;
+    }
+
+    ~raw_array() { free(_data); }
+
+    raw_array& operator=(raw_array&& e) {
+        if (this != &e) {
+            free(_data);
+            _data = e._data;
+            e._data = nullptr;
         }
         return *this;
     }
 
-    uint size() const { return m_size; }
-    uint capacity() const { return m_data.size(); }
+    raw_array& operator=(const raw_array& e) = delete;
+
+    T& operator[](size_t index) { return _data[index]; }
+
+    const T& operator[](size_t index) const { return _data[index]; }
+
+    void resize(size_t size) {
+        if (size == 0) {
+            free(_data);
+            _data = nullptr;
+            return;
+        }
+        T* data = reinterpret_cast<T*>(realloc(_data, sizeof(T) * size));
+        if (!data) throw std::bad_alloc();
+        _data = data;
+    }
+
+    // TODO potentially faster as it can avoid copying
+    void resize_no_copy(size_t size) {
+        resize(size);
+    }
+
+    const T* data() const { return _data; }
+    T* data() { return _data; }
+
+    void swap(raw_array& o) { std::swap(_data, o._data); }
+
+   private:
+    T* _data;
+};
+
+// TODO std::hash
+
+template <typename T, typename Size = uint, Size InitialCapacity = 4>
+class array_deque {
+   public:
+    array_deque() {}
+
+    array_deque(Size size) : m_data(size), m_capacity(size), m_size(size) {
+        FOR(i, m_size) new (&m_data[i]) T();
+    }
+
+    array_deque(Size size, const T& init) : m_data(size), m_capacity(size), m_size(size) {
+        FOR(i, m_size) new (&m_data[i]) T(init);
+    }
+
+    array_deque(const array_deque& o) : m_data(o.size()), m_capacity(o.size()), m_size(o.size()) {
+        FOR(i, m_size) new (&m_data[i]) T(o[i]);
+    }
+
+    array_deque(array_deque&& o) : m_data(std::move(o.m_data)), m_capacity(o.m_capacity), m_start(o.m_start), m_size(o.m_size) {
+        o.m_capacity = 0;
+        o.m_start = 0;
+        o.m_size = 0;
+    }
+
+    ~array_deque() {
+        FOR(i, m_size) m_data[offset(0)].~T();
+    }
+
+    auto operator=(const array_deque& o) {
+        if (this == &o) return *this;
+
+        // destroy all elements
+        FOR(i, m_size) m_data[offset(i)].~T();
+
+        if (o.size() > capacity()) {
+            m_data.resize_no_copy(o.size());
+            m_capacity = o.size();
+        }
+        m_start = 0;
+        m_size = o.size();
+
+        FOR(i, m_size) new (&m_data[i]) T(o[i]);
+        return *this;
+    }
+
+    auto operator=(array_deque&& o) {
+        if (this == &o) return *this;
+
+        m_data = std::move(o.m_data);
+        m_capacity = o.m_capacity;
+        m_start = o.m_start;
+        m_size = o.m_size;
+
+        o.m_capacity = 0;
+        o.m_start = 0;
+        o.m_size = 0;
+        return *this;
+    }
+
+    Size size() const { return m_size; }
+    Size capacity() const { return m_capacity; }
     bool empty() const { return m_size == 0; }
 
-    const T& operator[](uint index) const {
-        return (m_start + index < capacity()) ? m_data[m_start + index] : m_data[m_start + index - capacity()];
-    }
-    T& operator[](uint index) {
-        return (m_start + index < capacity()) ? m_data[m_start + index] : m_data[m_start + index - capacity()];
-    }
+    // TODO operator--
+    struct iterator {
+        array_deque& deque;
+        T* ptr;
 
-    void push_back(T value) {
-        if (m_size == m_data.size()) {
-            if (m_data.size() == 0) {
-                m_data.resize(4);
-            } else {
-                std::vector<T> data(m_data.size() * 2);
-                for (uint i = 0; i < m_size; i++) std::swap(data[i], operator[](i));
-                m_data.swap(data);
-            }
-            m_start = 0;
+        iterator(array_deque& d, T* p) : deque(d), ptr(p) {}
+
+        T& operator*() { return *ptr; }
+        iterator operator++() {
+            ptr += 1;
+            if (ptr == deque.data_end()) ptr = deque.data_begin();
+            return *this;
         }
-        uint end = m_start + m_size;
-        if (end >= capacity()) {
-            end -= capacity();
+        iterator operator++(int) {
+            ON_SCOPE_EXIT({
+                ptr += 1;
+                if (ptr == deque.data_end()) ptr = deque.data_begin();
+            });
+            return *this;
         }
-        m_data[end] = value;
+        bool operator==(const iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const iterator& o) const { return ptr != o.ptr; }
+    };
+
+    struct const_iterator {
+        const array_deque& deque;
+        const T* ptr;
+
+        const_iterator(const array_deque& d, const T* p) : deque(d), ptr(p) {}
+
+        const T& operator*() const { return *ptr; }
+        const_iterator operator++() {
+            ptr += 1;
+            if (ptr == deque.data_end()) ptr = deque.data_begin();
+            return *this;
+        }
+        const_iterator operator++(int) {
+            ON_SCOPE_EXIT({
+                ptr += 1;
+                if (ptr == deque.data_end()) ptr = deque.data_begin();
+            });
+            return *this;
+        }
+        bool operator==(const const_iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const const_iterator& o) const { return ptr != o.ptr; }
+    };
+
+    iterator begin() { return iterator(*this, &m_data[m_start]); }
+    iterator end() { return iterator(*this, &m_data[offset(m_size)]); }
+
+    const_iterator begin() const { return const_iterator(*this, &m_data[m_start]); }
+    const_iterator end() const { return const_iterator(*this, &m_data[offset(m_size)]); }
+
+    const T& operator[](Size index) const { return m_data[offset(index)]; }
+    T& operator[](Size index) { return m_data[offset(index)]; }
+
+    const T& front() const { return m_data[m_start]; }
+    T& front() { return m_data[m_start]; }
+
+    const T& back() const { return m_data[offset(m_size - 1)]; }
+    T& back() { return m_data[offset(m_size - 1)]; }
+
+    void push_back(T&& value) {
+        ensure_space();
+        Size i = offset(m_size);
+        new (&m_data[i]) T(std::move(value));
         m_size += 1;
     }
 
-    T pop_front() {
-        uint i = m_start++;
-        if (m_start >= capacity()) {
-            m_start -= capacity();
-        }
+    void push_back(const T& value) {
+        ensure_space();
+        Size i = offset(m_size);
+        new (&m_data[i]) T(value);
+        m_size += 1;
+    }
+
+    T pop_back() {
+        Size i = offset(m_size - 1);
         m_size -= 1;
-        return m_data[i];
+        ON_SCOPE_EXIT(m_data[i].~T());
+        return std::move(m_data[i]);
+    }
+
+    void push_front(T&& value) {
+        ensure_space();
+        Size i = ((m_start == 0) ? m_capacity : m_start) - 1;
+        new (&m_data[i]) T(std::move(value));
+        m_size += 1;
+        m_start = i;
+    }
+
+    void push_front(const T& value) {
+        ensure_space();
+        Size i = ((m_start == 0) ? m_capacity : m_start) - 1;
+        new (&m_data[i]) T(value);
+        m_size += 1;
+        m_start = i;
+    }
+
+    T pop_front() {
+        Size i = m_start++;
+        if (m_start >= capacity()) m_start -= capacity();
+        m_size -= 1;
+        ON_SCOPE_EXIT(m_data[i].~T());
+        return std::move(m_data[i]);
     }
 
     void swap(array_deque& o) {
         m_data.swap(o.m_data);
+        std::swap(m_capacity = o.m_capacity);
         std::swap(m_start, o.m_start);
         std::swap(m_size, o.m_size);
+    }
+
+    void reserve(Size new_capacity) {
+        if (new_capacity <= capacity()) return;
+
+        // resize in place if possible
+        if (m_start + m_size <= capacity()) {
+            m_data.resize(new_capacity);
+            m_capacity = new_capacity;
+            return;
+        }
+
+        raw_array<T> new_data(new_capacity);
+        FOR(i, m_size) {
+            T& e = m_data[offset(i)];
+            new (&new_data[i]) T(std::move(e));
+            e.~T();
+        }
+        m_data = std::move(new_data);
+        m_capacity = new_capacity;
+        m_start = 0;
     }
 
     void clear() {
@@ -87,8 +268,52 @@ class array_deque<T, std::enable_if_t<std::is_trivial_v<T>>> {
         m_size = 0;
     }
 
+    void shrink_to_fit() {
+        if (m_size == capacity()) return;
+
+        if (m_start == 0) {
+            m_data.resize(m_size);
+            m_capacity = m_size;
+            return;
+        }
+
+        raw_array<T> new_data(m_size);
+        FOR(i, m_size) {
+            T& e = m_data[offset(i)];
+            new (&new_data[i]) T(std::move(e));
+            e.~T();
+        }
+        m_data = std::move(new_data);
+        m_capacity = m_size;
+    }
+
+    bool operator==(const array_deque& o) const {
+        if (m_size != o.size()) return false;
+        FOR(i, m_size) if (!(operator[](i) == o.operator[](i))) return false;
+        return true;
+    }
+
+    bool operator!=(const array_deque& o) const { return !operator==(o); }
+
    private:
-    std::vector<T> m_data;
-    uint m_start = 0;
-    uint m_size = 0;
+    const T* data_begin() const { return m_data.data(); }
+    const T* data_end() const { return m_data.data() + capacity(); }
+    T* data_begin() { return m_data.data(); }
+    T* data_end() { return m_data.data() + capacity(); }
+
+    Size offset(Size index) const {
+        Size o = m_start + index;
+        if (o >= capacity()) o -= capacity();
+        return o;
+    }
+
+    void ensure_space() {
+        if (m_size == capacity()) reserve(std::max<Size>(InitialCapacity, round_up_power2(capacity() + 1)));
+    }
+
+   private:
+    raw_array<T> m_data;
+    Size m_capacity = 0;
+    Size m_start = 0;
+    Size m_size = 0;
 };
--- core/array_deque_test.cc
+++ core/array_deque_test.cc
@@ -1,3 +1,6 @@
+#include <vector>
+
+#include "fmt/core.h"
 #include "core/numeric.h"
 #include "core/array_deque.h"
 #define CATCH_CONFIG_MAIN
@@ -22,4 +25,214 @@ TEST_CASE("array_deque basic", "[array_deque]") {
     REQUIRE(deque.size() == 130);
 
     FOR(i, 130) REQUIRE(deque[i] == 40 + i);
+}
+
+using T = std::string;
+T ops;
+
+void Log(int a, std::string_view m) {
+    // fmt::print("{} {}\n", a, m);
+    ops += fmt::format("{} {}\n", a, m);
+}
+
+struct Payload {
+    int a;
+    Payload() : a(0) { Log(0, "P()"); }
+    Payload(int a) : a(a) { Log(a, "P(int)"); }
+    Payload(Payload&& o) : a(o.a) { Log(a, "P(P&&)"); o.a = 0; }
+    Payload(const Payload& o) : a(o.a) { Log(a, "P(const P&)"); }
+    ~Payload() { Log(a, "~P()"); }
+    void operator=(Payload&& o) { Log(a, "=(P&&)"); o.a = 0; }
+    void operator=(const Payload& o) { Log(a, "=(const P&)"); }
+    bool operator==(int o) const { return a == o; }
+    bool operator==(const Payload& o) const { return a == o.a; }
+};
+
+TEST_CASE("array_deque::array_deque()", "[array_deque]") {
+    ops.clear();
+    array_deque<Payload> q;
+    REQUIRE(ops == "");
+}
+
+TEST_CASE("array_deque::array_deque(uint)", "[array_deque]") {
+    ops.clear();
+    array_deque<Payload> q(3);
+    REQUIRE(ops == "0 P()\n0 P()\n0 P()\n");
+    REQUIRE(q.size() == 3);
+    REQUIRE(q.capacity() == 3);
+    REQUIRE(q.front() == 0);
+    REQUIRE(q.back() == 0);
+}
+
+TEST_CASE("array_deque::array_deque(uint, const T&)", "[array_deque]") {
+    const Payload a = 7;
+    ops.clear();
+    array_deque<Payload> q(3, a);
+    REQUIRE(ops == "7 P(const P&)\n7 P(const P&)\n7 P(const P&)\n");
+    REQUIRE(q.size() == 3);
+    REQUIRE(q.capacity() == 3);
+    REQUIRE(q.front() == 7);
+    REQUIRE(q.back() == 7);
+}
+
+TEST_CASE("array_deque::~array_deque()", "[array_deque]") {
+    auto* e = new array_deque<Payload>;
+    auto& q = *e;
+    q.push_back(4);
+    q.push_back(5);
+    q.push_back(6);
+    q.pop_front();
+    q.pop_back();
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+    REQUIRE(q.front() == 5);
+    REQUIRE(q.back() == 5);
+
+    ops.clear();
+    delete e;
+    REQUIRE(ops == "5 ~P()\n");
+}
+
+TEST_CASE("array_deque::shrink_to_fit(uint)", "[array_deque]") {
+    array_deque<Payload> q;
+    q.push_back(4);
+    q.push_back(5);
+    q.push_back(6);
+    q.pop_front();
+    q.pop_back();
+
+    ops.clear();
+    q.shrink_to_fit();
+    REQUIRE(ops == "5 P(P&&)\n0 ~P()\n");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 1);
+}
+
+TEST_CASE("array_deque::operator==(const array_deque&)", "[array_deque]") {
+    array_deque<Payload> q;
+    q.reserve(3);
+    q.push_back(4);
+    q.push_back(5);
+    q.pop_front();
+
+    array_deque<Payload> e;
+    e.push_back(5);
+    bool r = e == q;
+    REQUIRE(r);
+    r = q == e;
+    REQUIRE(r);
+
+    e.push_back(5);
+    r = e == q;
+    REQUIRE(!r);
+    r = q == e;
+    REQUIRE(!r);
+}
+
+TEST_CASE("array_deque::begin() end()", "[array_deque]") {
+    array_deque<Payload> q;
+    q.push_back(4);
+    q.push_back(5);
+    q.pop_front();
+    q.push_back(6);
+    q.pop_back();
+    q.push_back(7);
+
+    auto it = q.begin();
+    REQUIRE(it != q.end());
+    REQUIRE(*it == 5);
+    it++;
+    REQUIRE(it != q.end());
+    REQUIRE(*it == 7);
+    it++;
+    REQUIRE(it == q.end());
+}
+
+TEST_CASE("array_deque::begin() end() const", "[array_deque]") {
+    array_deque<Payload> q;
+    q.push_back(4);
+    q.push_back(5);
+    q.pop_front();
+    q.push_back(6);
+    q.pop_back();
+    q.push_back(7);
+
+    const auto& cq = q;
+    auto it = cq.begin();
+    REQUIRE(it != cq.end());
+    REQUIRE(*it == 5);
+    it++;
+    REQUIRE(it != cq.end());
+    REQUIRE(*it == 7);
+    it++;
+    REQUIRE(it == cq.end());
+}
+
+TEST_CASE("array_deque::reserve(uint)", "[array_deque]") {
+    array_deque<Payload> q;
+    q.push_back(4);
+    ops.clear();
+    q.reserve(1);
+    REQUIRE(ops == "");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+
+    ops.clear();
+    q.reserve(10);
+    REQUIRE(ops == "");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 10);
+}
+
+TEST_CASE("array_deque::push_back(T&&)", "[array_deque]") {
+    array_deque<Payload> q;
+    Payload a = 3;
+    ops.clear();
+    q.push_back(std::move(a));
+    REQUIRE(ops == "3 P(P&&)\n");
+
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+    REQUIRE(q[0] == 3);
+    REQUIRE(q.front() == 3);
+    REQUIRE(q.back() == 3);
+}
+
+TEST_CASE("array_deque::push_front(T&&)", "[array_deque]") {
+    array_deque<Payload> q;
+    Payload a = 3;
+    ops.clear();
+    q.push_front(std::move(a));
+    REQUIRE(ops == "3 P(P&&)\n");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+    REQUIRE(q[0] == 3);
+    REQUIRE(q.front() == 3);
+    REQUIRE(q.back() == 3);
+}
+
+TEST_CASE("array_deque::push_back(const T&)", "[array_deque]") {
+    array_deque<Payload> q;
+    const Payload a = 3;
+    ops.clear();
+    q.push_back(a);
+    REQUIRE(ops == "3 P(const P&)\n");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+    REQUIRE(q[0] == 3);
+    REQUIRE(q.front() == 3);
+    REQUIRE(q.back() == 3);
+}
+
+TEST_CASE("array_deque::push_front(const T&)", "[array_deque]") {
+    array_deque<Payload> q;
+    const Payload a = 3;
+    ops.clear();
+    q.push_front(a);
+    REQUIRE(ops == "3 P(const P&)\n");
+    REQUIRE(q.size() == 1);
+    REQUIRE(q.capacity() == 4);
+    REQUIRE(q[0] == 3);
+    REQUIRE(q.front() == 3);
+    REQUIRE(q.back() == 3);
 }
