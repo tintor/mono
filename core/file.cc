57a0ec5606fba96b4209e44b75d1d3ee10317a11
Marko Tintor <marko@tintor.net>
2020-07-04 17:12:53 -0700

    core: add [file]

---
+++ core/file.cc
@@ -0,0 +1,42 @@
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <iostream>
+
+#include "core/exception.h"
+#include "core/file.h"
+
+using namespace std::literals;
+
+MappedFile::MappedFile(std::string_view filename) {
+    std::string fname;
+    fname = filename;
+    m_fd = open(fname.c_str(), O_RDONLY);
+    if (m_fd == -1) THROW(runtime_error, "open");
+
+    struct stat sb;
+    if (fstat(m_fd, &sb) == -1) THROW(runtime_error, "fstat");
+
+    m_length = sb.st_size;
+    m_addr = static_cast<const char*>(mmap(NULL, m_length, PROT_READ, MAP_PRIVATE, m_fd, 0u));
+    if (m_addr == MAP_FAILED) THROW(runtime_error, "mmap");
+}
+
+MappedFile::~MappedFile() {
+    munmap(const_cast<char*>(m_addr), m_length);
+    close(m_fd);
+}
+
+std::string_view FileReader::readline() {
+    if (m_pos == m_file.view().end()) return ""sv;
+
+    const char* b = m_pos;
+    const char* e = m_file.view().end();
+
+    while (m_pos < e) {
+        if (*m_pos == '\n') return std::string_view(b, ++m_pos - b);
+        m_pos += 1;
+    }
+    return std::string_view(b, m_pos - b);
+}
---
+++ core/file.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string_view>
+#include <optional>
+
+#include "core/each.h"
+
+class MappedFile {
+   public:
+    MappedFile(std::string_view filename);
+    MappedFile(const MappedFile&) = delete;
+    MappedFile& operator=(const MappedFile&) = delete;
+    ~MappedFile();
+
+    std::string_view view() const { return std::string_view(m_addr, m_length); }
+
+   private:
+    int m_fd;
+    size_t m_length;
+    const char* m_addr;
+};
+
+// TODO this can just be a string iterator (detached from file)
+class FileReader : public each<FileReader> {
+   public:
+    FileReader(std::string_view filename) : m_file(filename), m_pos(m_file.view().begin()) {}
+
+    std::string_view readline();
+
+    // removes \n from the end
+    std::optional<std::string_view> next() {
+        auto line = readline();
+        if (line.empty()) return std::nullopt;
+        return line.substr(0, line.size() - 1);
+    }
+
+   private:
+    MappedFile m_file;
+    const char* m_pos;
+};
