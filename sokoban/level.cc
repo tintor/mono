04a979788c9102343264f8095e527d112cef6b45
Marko Tintor <marko@tintor.net>
2020-08-29 12:42:14 -0700

    sokoban: Make find_dead_cells() much faster

--- sokoban/level.cc
+++ sokoban/level.cc
@@ -154,22 +154,6 @@ struct Minimal {
     }
 
     uint find_dead_cells() {
-        uint count = 0;
-
-        // Mark all empty corner cells as dead.
-        for (int i = 0; i < cell.size(); i++) {
-            if (!empty(i)) continue;
-            for (int d = 0; d < 4; d++) {
-                int a = i + dirs[d];
-                int b = i + dirs[(d + 1) % 4];
-                if (!open(a) && !open(b)) {
-                    cell[i] = Code::Dead;
-                    count += 1;
-                    break;
-                }
-            }
-        }
-
         small_bfs<std::pair<ushort, ushort>> visitor(cell.size() * cell.size());
         const auto add = [this, &visitor](uint agent, uint box) {
             check(agent);
@@ -177,33 +161,28 @@ struct Minimal {
             visitor.add(std::pair<ushort, ushort>(agent, box), agent * cell.size() + box);
         };
 
+        vector<char> live(cell.size(), false);
         for (int i = 0; i < cell.size(); i++) {
-            if (!empty(i) || goal(i)) continue;
+            if (!goal(i)) continue;
+            for (int m : dirs) if (open(i + m)) add(i + m, i);
+            live[i] = true;
+        }
 
-            for (int m : dirs)
-                if (open(i + m)) add(i + m, i);
+        for (auto [agent, box] : visitor) {
+            for (int m : dirs) {
+                if (!open(agent + m)) continue;
+                if (agent + m != box) add(agent + m, box);
+                if (agent - m != box) continue;
+                live[agent] = true;
+                add(agent + m, agent);
+            }
+        }
 
-            bool dead = true;
-            for (auto [agent, box] : visitor) {
-                for (int m : dirs) {
-                    if (open(agent + m) && agent + m != box) {
-                        add(agent + m, box);
-                        continue;
-                    }
-                    if (open(agent + m) && agent + m == box && open(agent + m + m)) {
-                        if (goal(agent + m + m)) {
-                            visitor.clear();
-                            dead = false;
-                            break;
-                        }
-                        if (alive(agent + m + m) && cell[agent + m + m] != Code::Dead) add(agent + m, agent + m + m);
-                    }
-                }
-            }
-            if (dead) {
-                cell[i] = Code::Dead;
-                count += 1;
-            }
+        uint count = 0;
+        for (int i = 0; i < cell.size(); i++) {
+            if (live[i] || !open(i)) continue;
+            cell[i] = Code::Dead;
+            count += 1;
         }
         return count;
     }
