740d2f1968bfcf9bb5d726729b1f71b9ce21df39
Marko Tintor <marko@tintor.net>
2020-09-27 10:44:55 -0700

    [sokoban] refactor maximum_matching

--- sokoban/maximum_matching_test.cc
+++ sokoban/maximum_matching_test.cc
@@ -3,28 +3,28 @@
 #include "catch.hpp"
 
 TEST_CASE("maximum_matching EASY", "") {
-    BipGraph g(4, 4);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 2);
-    g.addEdge(4, 1);
-    REQUIRE(g.hopcroftKarp() == 4);
+    BipartiteGraph g(4, 4);
+    g.add_edge(1, 4);
+    g.add_edge(2, 3);
+    g.add_edge(3, 2);
+    g.add_edge(4, 1);
+    REQUIRE(g.maximum_matching() == 4);
 }
 
 TEST_CASE("maximum_matching B", "") {
-    BipGraph g(4, 4);
-    g.addEdge(1, 1);
-    g.addEdge(1, 2);
-    g.addEdge(1, 4);
-    g.addEdge(2, 2);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-    g.addEdge(4, 3);
-    REQUIRE(g.hopcroftKarp() == 3);
+    BipartiteGraph g(4, 4);
+    g.add_edge(1, 1);
+    g.add_edge(1, 2);
+    g.add_edge(1, 4);
+    g.add_edge(2, 2);
+    g.add_edge(2, 3);
+    g.add_edge(3, 3);
+    g.add_edge(4, 3);
+    REQUIRE(g.maximum_matching() == 3);
 }
 
 TEST_CASE("maximum_matching FULL", "") {
-    BipGraph g(4, 4);
-    for (int a = 1; a <= 4; a++) for (int b = 1; b <= 4; b++) g.addEdge(a, b);
-    REQUIRE(g.hopcroftKarp() == 4);
+    BipartiteGraph g(4, 4);
+    for (int a = 1; a <= 4; a++) for (int b = 1; b <= 4; b++) g.add_edge(a, b);
+    REQUIRE(g.maximum_matching() == 4);
 }
